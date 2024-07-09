cedbb1f73e35e820ab4113d3b823995c9a6e4e20
ADEK <adrianpodraza10@gmail.com>
2024-07-10 01:01:52 +0200

    final

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-icons/fa"],
+      external: ["react-icons/fa6"], // Add this line
     },
   },
 });
