c89f2957a8aaea6060b0bba2d286a8094cf1dc0c
ADEK <adrianpodraza10@gmail.com>
2024-07-31 16:22:32 +0200

    feat: change picture

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,15 +1,16 @@
 import Picture from "./../assets/Picture.png";
 import { Link } from "react-router-dom";
+
 function Gallery() {
   return (
-    <div className="snap-section relative flex h-screen flex-col  gap-24 p-16 xl:h-fit md:h-fit md:p-4 ">
+    <div className="snap-section relative flex h-screen flex-col  gap-24 p-16  md:p-4 ">
       <h1 data-aos="fade-up" className=" sacramento-regular text-8xl   ">
         Galeria
       </h1>
-      <div className="grid place-items-center ">
+      <div className="relative grid place-items-center">
         <Link to="galeria">
           <img
-            className=" picture swing w-[600px] hover:cursor-pointer"
+            className="picture swing  w-[600px] hover:cursor-pointer"
             src={Picture}
             alt="logobg"
           />
