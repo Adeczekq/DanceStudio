23d224379f472f7f9865c1a14a1741854411af81
ADEK <adrianpodraza10@gmail.com>
2024-07-31 16:22:06 +0200

    fix: fix issue with changeing displayed image/video on mobile devices

--- src/Pages/GalleryPage.jsx
+++ src/Pages/GalleryPage.jsx
@@ -1,13 +1,30 @@
 import { useState, useEffect } from "react";
 import { photos, videos } from "./media";
-import { FaArrowLeft, FaArrowRight, FaPlay } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaPlay, FaSpinner } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Measure from "react-measure";
+import { useSwipeable } from "react-swipeable";
+import useScreenType from "../hooks/useScreenTypes";
 
 function GalleryPage() {
   const [selectedMedia, setSelectedMedia] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const allMedia = [...photos, ...videos];
+  const handleLoad = () => setIsLoading(false);
+
+  const isMobile = useScreenType();
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => nextMedia(),
+    onSwipedRight: () => prevMedia(),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+    // Only allow swipe on mobile devices
+    filterTaps: true,
+    delta: 10, // Adjust this value based on your needs
+  });
 
   useEffect(() => {
     if (selectedMedia) {
@@ -41,39 +58,59 @@ function GalleryPage() {
   };
 
   return (
-    <div className="px-[100px] sm:px-4">
+    <div className="px-[100px] sm:px-8 " {...(isMobile ? swipeHandlers : {})}>
       <Link to="/">
         <FaArrowLeft className="fixed left-8 top-9 text-2xl sm:left-1" />
       </Link>
 
       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry gutter="5px">
-          {photos.map((photo) => (
-            <img
-              className="cursor-pointer"
-              key={photo.src}
-              src={photo.src}
-              alt="photo"
-              onClick={() => openModal(photo)}
-            />
-          ))}
-          {videos.map((video) => (
+          {[...photos, ...videos].map((media, i) => (
             <div
-              key={video.src}
-              onClick={() => openModal(video)}
-              className="relative flex cursor-pointer items-center justify-center"
+              key={i}
+              className="transition-all hover:scale-90"
+              onClick={() => openModal(media)}
             >
-              <video className="h-full w-full" poster={video.poster}>
-                <source src={video.src} type="video/mp4" />
-              </video>
-              <div className="absolute inset-0 flex items-center justify-center">
-                <FaPlay className="text-4xl text-white" />
-              </div>
+              <Measure>
+                {({ measureRef }) => (
+                  <>
+                    {media.type === "video" ? (
+                      <div
+                        ref={measureRef}
+                        className="relative flex cursor-pointer items-center justify-center"
+                      >
+                        <video
+                          className="h-full w-full "
+                          poster={media.poster}
+                          onLoadedData={handleLoad}
+                        >
+                          <source src={media.src} type="video/mp4" />
+                        </video>
+                        <div className="absolute inset-0 flex items-center justify-center">
+                          {isLoading && (
+                            <FaSpinner className="animate-spin text-4xl text-white" />
+                          )}
+                          {!isLoading && (
+                            <FaPlay className="text-4xl text-white" />
+                          )}
+                        </div>
+                      </div>
+                    ) : (
+                      <img
+                        ref={measureRef}
+                        src={media.src}
+                        alt="photo"
+                        className="cursor-pointer"
+                        onLoad={handleLoad}
+                      />
+                    )}
+                  </>
+                )}
+              </Measure>
             </div>
           ))}
         </Masonry>
       </ResponsiveMasonry>
-
       {selectedMedia && (
         <div className="fixed left-0 top-0 z-10 flex h-full w-full items-center justify-center bg-black">
           <div
@@ -82,11 +119,11 @@ function GalleryPage() {
           />
 
           <FaArrowLeft
-            className="absolute left-4 cursor-pointer text-4xl text-white"
+            className="absolute left-4  cursor-pointer text-4xl text-white md:hidden"
             onClick={prevMedia} // Przejdź do poprzedniego elementu
           />
           <FaArrowRight
-            className="absolute right-4 cursor-pointer text-4xl text-white"
+            className="absolute right-4  cursor-pointer text-4xl text-white md:hidden"
             onClick={nextMedia} // Przejdź do następnego elementu
           />
 
